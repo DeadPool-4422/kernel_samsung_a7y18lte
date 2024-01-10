3b4b0b934cd2e423289bca53f7ddb735c6332c92
DeadPool-4422 <dynamicplayer444@gmail.com>
2024-01-10 18:56:56 +0530

    Fixed the Firmware recursive Inclusion and updated the repo with the toolchain directly built into it using git submodule and added a gitignore for the out folder to stop the upload of build artifacts accidentally

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 2 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm64"
+#define UTS_VERSION "#2 SMP PREEMPT Wed Jan 10 18:41:51 IST 2024"
+#define LINUX_COMPILE_BY "dazai"
+#define LINUX_COMPILE_HOST "DESKTOP-MQ4D0VB"
+#define LINUX_COMPILER "gcc version 4.9.4 (Linaro GCC 4.9-2017.01) "
---
+++ include/generated/utsrelease.h
@@ -0,0 +1,1 @@
+#define UTS_RELEASE "4.4.177-g308a1b5d-dirty"
