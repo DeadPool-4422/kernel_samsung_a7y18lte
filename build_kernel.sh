54da9eda6b2783c6e4555b54322b7210e86f18c5
root <root@DESKTOP-MQ4D0VB>
2024-01-12 11:45:18 +0530

    Created the kernel-info.mk for the device and modified the defconfig for droidian

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 export ARCH=arm64
-export CROSS_COMPILE=./toolchain/bin/aarch64-linux-gnu-
 export ANDROID_MAJOR_VERSION=q
+export CROSS_COMPILE=./toolchain/bin/aarch64-linux-gnu-
 
 make exynos7885-a7y18lte_defconfig
 make -j$(nproc --all)
--- debian/kernel-info.mk
+++ debian/kernel-info.mk
@@ -8,23 +8,23 @@
 VARIANT = android
 
 # Kernel base version
-KERNEL_BASE_VERSION = 4.4-0
+KERNEL_BASE_VERSION = 4.14-177
 
 # The kernel cmdline to use
-KERNEL_BOOTIMAGE_CMDLINE = put-cmdline-here
+#KERNEL_BOOTIMAGE_CMDLINE = put-cmdline-here
 
 # Slug for the device vendor. This is going to be used in the KERNELRELASE
 # and package names.
-DEVICE_VENDOR = vendor
+DEVICE_VENDOR = samsung
 
 # Slug for the device model. Like above.
-DEVICE_MODEL = device1
+DEVICE_MODEL = a7y18lte
 
 # Slug for the device platform. If unsure, keep this commented.
-# DEVICE_PLATFORM = platform
+#DEVICE_PLATFORM = platform
 
 # Marketing-friendly full-name. This will be used inside package descriptions
-DEVICE_FULL_NAME = Vendor Device 1
+DEVICE_FULL_NAME = Samsung A7 2018
 
 # Whether to use configuration fragments to augment the kernel configuration.
 # If unsure, keep this to 0.
@@ -37,65 +37,52 @@ KERNEL_CONFIG_USE_DIFFCONFIG = 0
 
 # The diffconfig to apply. Only used when KERNEL_CONFIG_USE_DIFFCONFIG is
 # enabled.
-# KERNEL_PRODUCT_DIFFCONFIG = my_diffconfig
+#KERNEL_PRODUCT_DIFFCONFIG = my_diffconfig
 
 # Defconfig to use
-KERNEL_DEFCONFIG = defconfig
+KERNEL_DEFCONFIG = exynos7885-a7y18lte_defconfig
 
 # Whether to include DTBs with the image. Use 0 (no) or 1.
-# GKI devices should set this to 0
-KERNEL_IMAGE_WITH_DTB = 1
+KERNEL_IMAGE_WITH_DTB = 0
 
 # Path to the DTB
 # If you leave this undefined, an attempt to find it automatically
 # will be made.
-# KERNEL_IMAGE_DTB = arch/arm64/boot/dts/qcom/my_dtb.dtb
 
 # Whether to include a DTB Overlay. Use 0 (no) or 1.
-# GKI devices should set this to 0
-KERNEL_IMAGE_WITH_DTB_OVERLAY = 1
+KERNEL_IMAGE_WITH_DTB_OVERLAY = 0
 
 # Path to the DTB overlay.
 # If you leave this undefined, an attempt to find it automatically
 # will be made.
-# KERNEL_IMAGE_DTB_OVERLAY = arch/arm64/boot/dts/qcom/my_overlay.dtbo
+#KERNEL_IMAGE_DTB_OVERLAY = arch/arm64/boot/dts/qcom/my_overlay.dtbo
 
 # Whether to include the DTB Overlay into the kernel image
 # Use 0 (no, default) or 1.
 # dtbo.img will always be shipped in the linux-bootimage- package.
-# GKI devices should set this to 0
 KERNEL_IMAGE_WITH_DTB_OVERLAY_IN_KERNEL = 0
 
 # Path to a specifc configuration file for mkdtboimg.
 # The default is to leave it undefined.
-# KERNEL_IMAGE_DTB_OVERLAY_CONFIGURATION = debian/custom_dtbo_config.cfg
+#KERNEL_IMAGE_DTB_OVERLAY_CONFIGURATION = debian/custom_dtbo_config.cfg
 
 # Path to the DTB directory. Only define if KERNEL_IMAGE_DTB_OVERLAY_CONFIGURATION
 # is defined too.
-# KERNEL_IMAGE_DTB_OVERLAY_DTB_DIRECTORY = arch/arm64/boot/dts/qcom
-
-# Path to the prebuilt DT image. should only be defined on header version 1 and below. 
-# mostly used on samsung devices. default is to leave it undefined
-# KERNEL_PREBUILT_DT = debian/dt.img
+#KERNEL_IMAGE_DTB_OVERLAY_DTB_DIRECTORY = arch/arm64/boot/dts/qcom
 
 # Various other settings that will be passed straight to mkbootimg
-# GKI devices can leave BASE_OFFSET, KERNEL_OFFSET, INITRAMFS_OFFSET, SECOND_OFFSET and TAGS_OFFSET empty
-KERNEL_BOOTIMAGE_PAGE_SIZE = 4096
-KERNEL_BOOTIMAGE_BASE_OFFSET = 0x00000000
+KERNEL_BOOTIMAGE_PAGE_SIZE = 2048
+KERNEL_BOOTIMAGE_BASE_OFFSET = 0x10000000
 KERNEL_BOOTIMAGE_KERNEL_OFFSET = 0x00008000
 KERNEL_BOOTIMAGE_INITRAMFS_OFFSET = 0x01000000
 KERNEL_BOOTIMAGE_SECONDIMAGE_OFFSET = 0x00f00000
 KERNEL_BOOTIMAGE_TAGS_OFFSET = 0x00000100
 
 # Specify boot image security patch level if needed
-# KERNEL_BOOTIMAGE_PATCH_LEVEL = 2022-04-05
-
-# Specify boot image OS version if needed
-# KERNEL_BOOTIMAGE_OS_VERSION = 12.0.0
+#KERNEL_BOOTIMAGE_PATCH_LEVEL = 2022-04-05
 
 # Required for header version 2, ignore otherwise
-# GKI devices should leave this offset empty
-KERNEL_BOOTIMAGE_DTB_OFFSET = 0x1f00000
+#KERNEL_BOOTIMAGE_DTB_OFFSET = 0x1f00000
 
 # Kernel bootimage version. Defaults to 0 (legacy header).
 # As a rule of thumb:
@@ -103,14 +90,7 @@ KERNEL_BOOTIMAGE_DTB_OFFSET = 0x1f00000
 # Devices launched with Android 9: version 1
 # Devices launched with Android 10: version 2
 # Devices launched with Android 11: version 2 or 3 (GKI)
-# Devices launched with Android 12: version 3 (GKI) or 4 (GKI)
-# Devices launched with Android 13: version 3 (GKI) or 4 (GKI)
 KERNEL_BOOTIMAGE_VERSION = 0
-
-# Kernel initramfs compression. Defaults to gzip.
-# All non-gki devices need a gzip (gz) initramfs
-# For devices launched with a GKI kernel it can be either gzip (gz) or lz4 (lz4)
-KERNEL_INITRAMFS_COMPRESSION = gz
 
 ########################################################################
 # Android verified boot
@@ -119,7 +99,7 @@ KERNEL_INITRAMFS_COMPRESSION = gz
 # Whether to build a flashable vbmeta.img. Please note that currently
 # only empty vbmeta images (disabling verified boot) can be generated.
 # Use 0 (no) or 1 (default).
-DEVICE_VBMETA_REQUIRED = 1
+DEVICE_VBMETA_REQUIRED = 0
 
 # Samsung devices require a special flag. Enable the following if your
 # device is a Samsung device that requires flag 0 to be present
@@ -144,34 +124,26 @@ FLASH_IS_AONLY = 0
 # on older devices.
 #
 # Do not enable if you don't know what you're doing
-FLASH_IS_LEGACY_DEVICE = 0
-
-# On some exynos devices partition names are capitalized (boot is BOOT and so on)
-# This flag makes the kernel to get flashed to the correct partition on updates.
-FLASH_IS_EXYNOS = 0
-
-# On some devices flashing userdata.img via fastboot fails and telnet should
-# be used instead. Set to 1 for telnet and 0 for fastboot
-FLASH_USE_TELNET = 0
+FLASH_IS_LEGACY_DEVICE = 1
 
 # Device manufacturer. This must match the `ro.product.vendor.manufacturer`
 # Android property. If you don't want to specify this, leave it undefined,
 # FLASH_INFO_CPU will be checked instead.
-FLASH_INFO_MANUFACTURER = Vendor
+FLASH_INFO_MANUFACTURER = samsung
 
 # Device model. This must match the `ro.product.vendor.model`
 # Android property. If you don't want to specify this, leave it undefined,
 # FLASH_INFO_CPU will be checked instead.
-FLASH_INFO_MODEL = Device1
+FLASH_INFO_MODEL = SM-A750F
 
 # Device CPU. This will be grepped against /proc/cpuinfo to check if
 # we're running on the specific device. Note this is a last-resort
 # method, specifying FLASH_INFO_MANUFACTURER and FLASH_INFO_MODEL is
 # recommended.
-FLASH_INFO_CPU = My Fictional System-on-a-Chip
+#FLASH_INFO_CPU = My Fictional System-on-a-Chip
 
 # Space-separated list of supported device ids as reported by fastboot
-FLASH_INFO_DEVICE_IDS = model1 model2
+FLASH_INFO_DEVICE_IDS = SM-A750F a7y18lte
 
 ########################################################################
 # Kernel build settings
@@ -182,7 +154,6 @@ BUILD_CROSS = 1
 
 # (Cross-build only) The build triplet to use. You'll probably want to
 # use aarch64-linux-android- if building Android kernels.
-# might need to use aarch64-linux-gnu- with newer kernels such as GKI
 BUILD_TRIPLET = aarch64-linux-android-
 
 # (Cross-build only) The build triplet to use with clang. You'll probably
@@ -190,10 +161,7 @@ BUILD_TRIPLET = aarch64-linux-android-
 BUILD_CLANG_TRIPLET = aarch64-linux-gnu-
 
 # The compiler to use. Recent Android kernels are built with clang.
-BUILD_CC = clang
-
-# Use llvm instead of gcc. Recent Android kernels (past android 12) need llvm.
-BUILD_LLVM = 0
+BUILD_CC = aarch64-linux-android-gcc-4.9
 
 # Extra paths to prepend to the PATH variable. You'll probably want
 # to specify the clang path here (the default).
@@ -214,4 +182,4 @@ DEB_BUILD_FOR = arm64
 KERNEL_ARCH = arm64
 
 # Kernel target to build
-KERNEL_BUILD_TARGET = Image.gz
+KERNEL_BUILD_TARGET = Image
